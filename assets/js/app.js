8b1ed601fb59113e5951a012da77650655324228
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-02 14:52:01 -0800

    created three variable that story the value from the separate buttons in the dom and created three functions that take those values and print them to the console for testing purposes

---
+++ assets/js/app.js
@@ -0,0 +1,21 @@
+
+let rockButton = document.getElementById("rockButton")
+// let paperButton = document.getElementById("paperButton")
+// let scissorsButton = document.getElementById("scissorsButton")
+
+let testArr = []
+
+rockButton.onclick = function () {
+    console.log(rockButton.value)
+}
+
+paperButton.onclick = function () {
+    console.log(paperButton.value)
+}
+
+scissorsButton.onclick = function () {
+    console.log(scissorsButton.value)
+}
+
+
+
