276c7ea64094799ffddc0d581d6978f15d597598
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-02 22:52:21 -0800

    created a variable that stores the computer's choice for the game a variable that stores the user's choice

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,21 +1,32 @@
 
-let rockButton = document.getElementById("rockButton")
+const rockButton = document.getElementById("rockButton")
 // let paperButton = document.getElementById("paperButton")
 // let scissorsButton = document.getElementById("scissorsButton")
+const computerOptions = ["Rock", "Paper", "Scissors" ]
 
-let testArr = []
+let humanChoice = ""
+let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
+
+console.log(computerChoice)
 
 rockButton.onclick = function () {
     console.log(rockButton.value)
+    humanChoice = rockButton.value
 }
 
 paperButton.onclick = function () {
     console.log(paperButton.value)
+    humanChoice = rockButton.value
 }
 
 scissorsButton.onclick = function () {
     console.log(scissorsButton.value)
+    humanChoice = rockButton.value
 }
 
 
-
+if(humanChoice === computerChoice){
+    console.log("You win")
+}else {
+    console.log("you lose")
+}
