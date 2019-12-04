22c356d908df83b89dac9abd81d628922c687e98
BrandonMGH <BrandonMGH@Yahoo.com>
2019-12-04 13:46:16 -0800

    created some photoshop images that move using javascript functions according to score differential

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,6 +7,7 @@ const computerOptions = ["Rock", "Paper", "Scissors" ]
 
 let humanTally = 0; 
 let computerTally = 0; 
+let tallyDifference = 0
 let humanChoice = ""
 let computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
 
@@ -40,20 +41,53 @@ let compareChoices = function () {
 if(humanChoice === "Rock"  && computerChoice === "Scissors" || humanChoice === "Paper" && computerChoice === "Rock" || humanChoice === "Scissors" && computerChoice === "Paper"){
     alert("You win")
     humanTally++;
+    tallyDifference ++; 
     document.getElementById("humanTally").innerHTML = humanTally
-    document.getElementById("test").style.marginLeft = "60%";
     computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     console.log(computerChoice)
+    console.log(tallyDifference)
+    iconMove(); 
 }else if (humanChoice === computerChoice){
     alert("You both tied")
     computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     console.log(computerChoice)
+    console.log(tallyDifference)
 } else {
     alert("you lose")
     computerTally++;
+    tallyDifference--;
     document.getElementById("computerTally").innerHTML = computerTally
     computerChoice = computerOptions[Math.floor(Math.random()*computerOptions.length)];
     console.log(computerChoice)
+    console.log(tallyDifference)
+    iconMove();
 }
 };
 
+let iconMove = function (){
+    if(tallyDifference === -5){
+        document.getElementById("test").style.marginRight = "75%";
+    }else if (tallyDifference === -4){
+        document.getElementById("test").style.marginRight = "60%";
+    }else if (tallyDifference === -3){
+        document.getElementById("test").style.marginRight = "45%";
+    }else if (tallyDifference === -2){
+        document.getElementById("test").style.marginRight = "30%";
+    }else if (tallyDifference === -1){
+        document.getElementById("test").style.marginRight = "15%";
+    }else if (tallyDifference === -0){
+        document.getElementById("test").style.marginRight = "0%";
+        document.getElementById("test").style.marginLeft = "0%";
+    }else if (tallyDifference === 1){
+        document.getElementById("test").style.marginLeft = "15%";
+    }else if (tallyDifference === 2){
+        document.getElementById("test").style.marginLeft = "30%";
+    }else if (tallyDifference === 3){
+        document.getElementById("test").style.marginLeft = "45%";
+    }else if (tallyDifference === 4){
+        document.getElementById("test").style.marginLeft = "60%";
+    }else if (tallyDifference === 5){
+        document.getElementById("test").style.marginRight = "75%";
+    }
+}
+console.log(tallyDifference)
